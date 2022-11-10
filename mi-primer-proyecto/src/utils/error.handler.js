d11aef64857bcab20c980f449113bbf1a3a1a180
Kevin <kprimo@solera.pe>
2022-11-10 11:09:08 -0500

    Eliminar cache gitignore

--- mi-primer-proyecto/src/utils/error.handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHttp = void 0;
-const handleHttp = (response, error, errorRaw) => {
-    console.log(errorRaw);
-    response.status(500);
-    response.send({ error });
-};
-exports.handleHttp = handleHttp;
